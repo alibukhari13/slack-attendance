ef9d95bf21a9ccc83b96b22b700de3f41191a8d6
shameelnasir971 <shameel@frenco.ae>
2026-02-09 00:08:26 +0500

    fixes

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   projectId: "db-manager-86b40",
   storageBucket: "db-manager-86b40.firebasestorage.app",
   messagingSenderId: "25423726916",
-  appId: "1:25423726916:web:393f397dc581ec8eeb6f11"
+  appId: "1:25423726916:web:4feec5cefcd28126eb6f11"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
